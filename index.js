eefa6061391206222499e29025def7e98ccf21c0
zilola <rahimovzavohir1@gmail.com>
2025-11-03 17:53:48 +0500

    lambargini-urus

--- index.js
+++ index.js
@@ -2,19 +2,45 @@ import TelegramBot from "node-telegram-bot-api";
 
 const TOKEN = "8488685308:AAGylyRNm7hO_BhJl2nT4Rx_RlgEUYGGpIg"
 const bot = new TelegramBot (TOKEN, {polling: true});
+const lamboPhotoURL = "./images/Lambargini_urus.jpg"
 
-bot.on("message", function(msg) {
-    const chatId = msg.chat.id;
-    const text = msg.text;
-    const firstname = msg.chat.first_name;
-    if (text == "/start")
-    bot.sentMessage(chatId, `Xush kelibsiz, ${firstname}`,{
-        reply_markup: {
-
-        }
+bot.on("message", async function (msg) {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+  const firstname = msg.chat.first_name;
+  if (text == "/start") {
+    bot.sendMessage(chatId,` Xush kelibsiz, ${firstname}`, {
+      reply_markup: {
+        keyboard: [
+          [{ text: "Boshlash 🔥" }],
+          [{ text: "Menu 🥩" }, { text: "Sozlamalar ⚙️" }],
+        ],
+        resize_keyboard: true,
+      },
     });
+  } else if (text == "Boshlash 🔥") {
+    const xabar = await bot .sendMessage(chatId, "Iltmos kuting....");
+
+    setTimeout(function() {
+      bot.deleteMessage(chatId, xabar.message_id);
+      bot.sendPhoto(chatId, lamboPhotoURL ,{
+        caption:`
+        🦅 Lamborghini Urus:
+
+Lamborghini Urus is a luxury high-performance SUV made by the Italian car manufacturer Lamborghini.
+ It was first introduced in 2018 and is known as one of the fastest SUVs in the world.`
+      })
+    })
+    bot.sendMessage(chatId, "Boshlanyaptii....");
+  } else if (text == "Menu 🥩") {
+    bot.sendMessage(chatId, "Menyuga xush kelibsiz....");
+  } else if (text == "Sozlamalar ⚙️") {
+    bot.sendMessage(chatId, "Sozlamalar xush kelibsiz ⚙️....");
+  } else {
+    bot.sendMessage(chatId, "❗️ Xatolik, iltimos /start tugmasini bosing... ");
+  }
 });
 
-
 // npm install nodemon --save-dev
-console.log("bot ishga tushdi");
+
+console.log("Bot ishga tushdi  ");
